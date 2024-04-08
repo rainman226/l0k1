5d631025ddfbace78f873d6fb9686b47ab200cfc
rainman226 <dota2romaniarobi@gmail.com>
2024-04-08 21:40:53 +0300

    beta preparations

--- src/main/java/ro/uvt/loki/Main.java
+++ src/main/java/ro/uvt/loki/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.opencv.opencv_java;
 import org.opencv.core.Core;
 
 import java.io.IOException;
@@ -11,7 +13,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Loader.load(opencv_java.class);
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Loki");
